5280f199ee8371b663e2f7d4748af9fd1e2f757a
Erica Winberry <elaewin@gmail.com>
2017-04-03 11:30:36 -0700

    this is taking a ridiculously long time to get right.

---
+++ GoGoGithub/GoGoGithub/Credentials.swift
@@ -0,0 +1,13 @@
+//
+//  Credentials.swift
+//  GoGoGithub
+//
+//  Created by Erica Winberry on 4/3/17.
+//  Copyright © 2017 Erica Winberry. All rights reserved.
+//
+
+import Foundation
+
+let kGitHubClientID = "a5dedb2230658f9e92ce"
+let kGitHubClientSecret = "84d2e138cc532b373d73384b30ecfd507250eeb6"
+
