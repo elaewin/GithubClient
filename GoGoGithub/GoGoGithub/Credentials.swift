f3a93f9f6cf67cbc4aed99a5bb6120bba03e8187
Erica Winberry <elaewin@gmail.com>
2017-04-03 11:23:38 -0700

    add Credentials.swift to gitignore.

---
+++ GoGoGithub/GoGoGithub/Credentials.swift
@@ -0,0 +1,9 @@
+//
+//  Credentials.swift
+//  GoGoGithub
+//
+//  Created by Erica Winberry on 4/3/17.
+//  Copyright © 2017 Erica Winberry. All rights reserved.
+//
+
+import Foundation
---
+++ GoGoGithub/GoGoGithub/GitHub.swift
@@ -0,0 +1,9 @@
+//
+//  GitHub.swift
+//  GoGoGithub
+//
+//  Created by Erica Winberry on 4/3/17.
+//  Copyright © 2017 Erica Winberry. All rights reserved.
+//
+
+import Foundation
